e157f3a51f0f9ae09f5e97c1b888a355c2032636
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-05-29 17:55:01 -0400

    twilio testing

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -3,13 +3,17 @@ class PollsController < ApplicationController
   def index
     # let's pretend that we've mapped this action to 
     # http://localhost:3000/sms in the routes.rb file
-    
-    # localtunnel -k ~/.ssh/id_rsa.pub 8080
 
-    @message_body = params["Body"]
-    from_number = params["From"]
-    render @polls, :locals => {:message_body => @message_body}
- 
+    # localtunnel -k ~/.ssh/id_rsa.pub 8080
+    # @message_body = params["Body"]
+    # from_number = params["From"]
+    # render @polls, :locals => {:message_body => @message_body}
+    # # render text: params
+    # SMSLogger.log_text_message from_number, message_body
+    @city = params[:FromCity].capitalize
+    @state = params[:FromState]
+    render 'process_sms.xml.erb', :content_type => 'text/xml'
+
   end
 
   def show
