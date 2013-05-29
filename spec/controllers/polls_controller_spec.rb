bfce48f498f2e3ddcd932759f32460efab805435
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-05-29 17:17:12 -0400

    first commmit

---
+++ spec/controllers/polls_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+TWILIO_SID = "0923408203840918302481203948012837"
+TWILIO_AUTH = "629876927863496102023472376432234"
+TWILIO_NUMBER = "+15552716628"
+
+describe PollsController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+end
