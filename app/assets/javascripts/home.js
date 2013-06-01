83eacf2847960c09388a29ac59ae4f0f0f310c29
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-05-31 22:43:25 -0400

    slideToggling sign up login forms on homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+  $('a#signup').click(function(){
+    $('.registration').slideToggle('slow');
+    $('.login').hide();
+  });
+
+  $('a#login').click(function(){
+    $('.login').slideToggle('slow');
+    $('.registration').hide();
+  });
+
+});
