b7011aa095c74cb1f6fdc7409d9a7d57be890a36
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-06-04 23:42:59 -0400

    nesting the questions form inside the poll form

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,8 +2,10 @@ class Poll < ActiveRecord::Base
   attr_accessible :description, :end_time, :start_time, :title, :user_id
 
   belongs_to :user
-  has_many :questions
+  has_many :questions, :dependent => :destroy
   # This is the plularized form of sms, it's not smss
   has_many :sms, :through => :questions 
   has_many :feedbacks
+
+  accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,10 @@ class Question < ActiveRecord::Base
   attr_accessible :poll_id, :title
 
   belongs_to :poll
-  has_many :answers
+  has_many :answers, :dependent => :destroy
   # THis is the plularized form of sms, it's not smss
   has_many :sms
+                                          #If someone creates a form with a blank question field at the end, this will prevent it from being rendered on teh show page
+  accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank?}
+
 end
