9ade60e95f5640753a39ab192845b4b5e1fa4171
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-06-08 19:08:10 -0400

    adding a table to the show page, adjusting how some some data is rendered

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :is_correct, :question_id, :title, :sms_answer_code
+  attr_accessible :is_correct, :question_id, :title, :sms_answer_code, :votes
 
   belongs_to :question
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608164744) do
+ActiveRecord::Schema.define(:version => 20130608223936) do
 
   create_table "answers", :force => true do |t|
     t.text     "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130608164744) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "sms_answer_code"
+    t.integer  "votes"
   end
 
   create_table "feedbacks", :force => true do |t|
